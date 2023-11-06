b9aafac3d819e75941b3c5818fa669b0524f28d5
aluno <aluno@ifpe>
2023-11-06 16:19:05 -0300

    adicionando arquivos

---
+++ src/main/java/br/com/ifpe/oxelixoapi/OxeLixoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxelixoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxeLixoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxeLixoApiApplication.class, args);
+	}
+
+}
